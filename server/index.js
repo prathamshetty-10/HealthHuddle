4ebaed72ef4e45b7fefc2898dcacd2fb4ab03aad
Pratham shetty <prathamshetty484@gmail.com>
2024-06-27 23:19:25 +0530

    most of the work is done apart from responsiveness

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ const server=app.listen(process.env.PORT,()=>{
 
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://health-huddle.vercel.app/",
         
         methods: ["GET", "POST"],
 
